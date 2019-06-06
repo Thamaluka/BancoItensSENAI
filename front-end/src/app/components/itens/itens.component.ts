ed78b328b1d91785a7451220b0e8559670ff3277
Thamires <thamires_garcia@estudante.sc.senai.br>
2019-06-05 21:29:44 -0300

    create item

--- front-end/src/app/components/itens/itens.component.ts
+++ front-end/src/app/components/itens/itens.component.ts
@@ -8,24 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class ItensComponent implements OnInit {
 
   constructor() { }
-  ckeditorContent: string = '<p>Some html</p>';
-  typeObj: string = 'Enunciado'
+  fileData: File = null;
+
   cabecalho = [
-    { text: null, image: null, type: null }
+    { id: 1, name: 'Enunciado', text: null, image: null, type: null },
+    { id: 2, name: 'Suporte', text: null, image: null, type: null },
+    { id: 3, name: 'Comando', text: null, image: null, type: null }
   ]
 
   ngOnInit() {
   }
 
-  addType = (index: number, type: string) => {
-    this.cabecalho[index].type = type;
-    /*   const newItem = {
-        text: null, image: null, type: null 
-      }
-      this.cabecalho.push(newItem) */
+  imgProgress(fileInput: any, index: number) {
+    const file = <File>fileInput.target.files[0];
+    this.cabecalho[index].type = "image";
+    this.cabecalho[index].image = file;
+    const reader = new FileReader();
+    reader.onload = e => this.cabecalho[index].image = reader.result;
+    reader.readAsDataURL(file);
   }
 
-  removeType = (index: number,) => {
+  teste() {
+    console.log("AUI")
+  }
+
+  addType = (index: number, type: string) => {
+    this.cabecalho[index].type = type;
+  }
+
+  removeType = (index: number, ) => {
     this.cabecalho[index].type = null;
   }
 
